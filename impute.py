267471ddd0708ab30a47ff44acd38ca21b55c845
Cristian Groza <cristian.groza@protonmail.com>
2019-11-14 21:11:09 -0500

    Comments.

--- impute.py
+++ impute.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import sys
 
+# load data
 tissue = os.path.join("simulated_data", "TWAS", "tissue0")
 genotypes = pd.read_csv(os.path.join("simulated_data", "genotype", "mini_test_genotype.csv"))
 n_samples = len(genotypes)
@@ -14,12 +15,17 @@ for gene_csv in os.listdir(tissue):
     eqtls = eqtls.set_index("Unnamed: 0").T
     genes[os.path.splitext(os.path.basename(gene_csv))[0]] = eqtls
 
+# will contain imputed gene expression for each gene in each sample
 imputed_expression = {}
 # impute
 for gene in genes:
+    # baseline expression is 0
     imputed_expression[gene] = [0] * n_samples
+    # add the effect of each SNP to the baseline gene expression
     for snp in genes[gene]:
+        # effect of eQTL on gene expression
         eqtl_effect = genes[gene][snp]["w_hat"]
+        # add the effect of eQTL weighted by genotype for each sample
         for sample in range(n_samples):
             imputed_expression[gene][sample] = imputed_expression[gene][sample] + genotypes[snp][sample] * eqtl_effect
 
